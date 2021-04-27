470a59db62f700d53eae47274db7aa2f0554373b
Long Le <lehuynhlong997@gmail.com>
2021-04-27 16:41:23 +0700

    init backend project and implement local login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,7 @@
-"""backend_src URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.accounts.urls import accounts_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# add accounts urls to urlpatterns
+urlpatterns += accounts_urlpatterns
--- backend/backend_src/fashion_info/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FashionInfoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fashion_info'
