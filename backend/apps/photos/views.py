683224f341c3dc5621a5bcf67b5c8f22e5733923
vlttvu <vlttvu@mediba.jp>
2021-05-03 00:02:39 +0700

    [Update] add api view photo

--- backend/apps/photos/views.py
+++ backend/apps/photos/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-from .models import photo
+from .models import Photo
+from .serializers import PhotoSerializer
 
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+
 # Create your views here.
 
 
-class photoList(APIView):
-    """
-    List all photos, or create a new photo.
-    """
-    def get(self, request, format=None):
-        photos = photo.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+class PhotoList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
