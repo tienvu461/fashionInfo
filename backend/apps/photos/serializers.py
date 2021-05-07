a073392169f1b90e8b9baeebabd87f5ac7c597a8
Tien Vu <tienvu461@gmail.com>
2021-05-07 23:03:16 +0700

    [Update] change url, add field activities and serializers

--- backend/apps/photos/serializers.py
+++ backend/apps/photos/serializers.py
@@ -1,8 +1,72 @@
 from rest_framework import serializers
-from .models import Photo
+from .models import Photo, News, Like, Dislike, Comment
+from .consts import modelConst, postTypeEnum
 
 
 class PhotoSerializer(serializers.ModelSerializer):
+    activities = serializers.SerializerMethodField()
     class Meta:
         model = Photo
-        fields = ['id', 'title', 'author', 'image_path', 'status',]
+        fields = ['id', 'title', 'author', 'image_path', 'status', 'created_at', 'activities']
+
+    def to_representation(self, instance):
+        data_fields = super(PhotoSerializer, self).to_representation(instance)
+        data_fields['created_at'] = int(instance.created_at.timestamp())
+
+        
+        like = serializers.RelatedField(source='like', read_only=True)
+
+        return data_fields
+
+    def get_activities(self, instance):
+        like_num = Like.objects.filter(post_id=instance.id, post_type=postTypeEnum.Photo.value).count()
+        dislike_num = Dislike.objects.filter(post_id=instance.id, post_type=postTypeEnum.Photo.value).count()
+        comment_num =  Comment.objects.filter(post_id=instance.id, post_type=postTypeEnum.Photo.value).count()
+
+        return {'likes': like_num,
+                'dislikes': dislike_num,
+                'comments': comment_num
+        }
+
+    # def get_likes(self, instance):
+    #     return Like.objects.filter(post_id=instance.id).count()
+
+    # def get_dislikes(self, instance):
+    #     return Dislike.objects.filter(post_id=instance.id).count()
+
+    # def get_comments(self, instance):
+    #     return Comments.objects.filter(post_id=instance.id).count()
+
+class NewsSerializer(serializers.ModelSerializer):
+    activities = serializers.SerializerMethodField()
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'author', 'formatted_markdown', 'status', 'created_at', 'activities']
+
+    def to_representation(self, instance):
+        data_fields = super(NewsSerializer, self).to_representation(instance)
+        data_fields['created_at'] = int(instance.created_at.timestamp())
+
+        return data_fields
+        
+    def get_activities(self, instance):
+        like_num = Like.objects.filter(post_id=instance.id, post_type=postTypeEnum.News.value).count()
+        dislike_num = Dislike.objects.filter(post_id=instance.id, post_type=postTypeEnum.News.value).count()
+        comment_num =  Comment.objects.filter(post_id=instance.id, post_type=postTypeEnum.News.value).count()
+
+        return {'likes': like_num,
+                'dislikes': dislike_num,
+                'comments': comment_num
+        }
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = News
+        fields = ['id', 'title', 'author', 'formatted_markdown', 'status', 'created_at']
+
+    def to_representation(self, instance):
+        data_fields = super(NewsSerializer, self).to_representation(instance)
+        data_fields['created_at'] = int(instance.created_at.timestamp())
+
+        return data_fields
+        
--- backend/apps/photos/views.py
+++ backend/apps/photos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from .models import Photo
-from .serializers import PhotoSerializer
+from .models import Photo, News, Like, Comment
+from .serializers import PhotoSerializer, NewsSerializer
 import logging
 from django.http import Http404
 from rest_framework.views import APIView
@@ -13,10 +13,60 @@ from rest_framework import generics
 
 logger = logging.getLogger('photos')
 
+# class PhotoFilter(filters.FilterSet):
+#     price = filters.NumberFilter(name="price", lookup_expr='lte')
+#     features = filters.ModelMultipleChoiceFilter(
+#         name="features", 
+#         conjoined=True,
+#         queryset=Features.objects.all()
+#         )
+
+#     class Meta:
+#         model = Room
+#         fields = ['price', 'features']
+
 class PhotoList(generics.ListCreateAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    # filter_backends = (filters.DjangoFilterBackend,)
+    # filter_class = PhotoFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = Photo.objects.all()
+        # queryset = queryset.order_by('-created_at')
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+            
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+
+
+class NewsList(generics.ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+    # filter_backends = (filters.DjangoFilterBackend,)
+    # filter_class = NewsFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = News.objects.all()
+        # queryset = queryset.order_by('-created_at')
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+            
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class NewsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
