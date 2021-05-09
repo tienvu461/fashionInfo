f01f5e7dc57a0c55afdb3efe06980ab649912831
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-05-10 00:13:20 +0700

    Add 404 not found page

--- frontend/front/src/App.tsx
+++ frontend/front/src/App.tsx
@@ -6,6 +6,7 @@ import HeaderMenu from './components/HeaderMenu';
 import ForumPage from './pages/ForumPage';
 import HomePage from './pages/HomePage';
 import NewsPage from './pages/NewsPage';
+import NotFound from './pages/NotFound';
 
 function App(): JSX.Element {
   return (
@@ -15,6 +16,7 @@ function App(): JSX.Element {
           <Route component={HomePage} exact path='/' />
           <Route component={NewsPage} exact path='/news' />
           <Route component={ForumPage} exact path='/forum' />
+          <Route component={NotFound} />
         </Switch>
       </HeaderMenu>
     </div>
