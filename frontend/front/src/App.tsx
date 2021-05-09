b3ae87f44d0c60e6bec341496ecba2417da71ce2
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-05-10 00:10:35 +0700

    Add switch route

--- frontend/front/src/App.tsx
+++ frontend/front/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.scss';
 import HeaderMenu from './components/HeaderMenu';
+import ForumPage from './pages/ForumPage';
 import HomePage from './pages/HomePage';
+import NewsPage from './pages/NewsPage';
 
 function App(): JSX.Element {
   return (
     <div className='App'>
       <HeaderMenu>
-        <HomePage />
+        <Switch>
+          <Route component={HomePage} exact path='/' />
+          <Route component={NewsPage} exact path='/news' />
+          <Route component={ForumPage} exact path='/forum' />
+        </Switch>
       </HeaderMenu>
     </div>
   );
