3cbda82e4f77026fc7ce991d9b7a5f29dfaaab83
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-05-13 23:26:56 +0700

    Add and update actions photo

--- frontend/front/src/pages/HomePage/components/Photo/useStyles.ts
+++ frontend/front/src/pages/HomePage/components/Photo/useStyles.ts
@@ -3,7 +3,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import { gray, pdXL, pdSM, pdLG, pdMD, theme } from '../../../../styles/theme';
 
 const useStyles: () => Record<
-  'root' | 'card' | 'paper' | 'picture' | 'gridItem',
+  | 'root'
+  | 'card'
+  | 'paper'
+  | 'picture'
+  | 'gridItem'
+  | 'actions'
+  | 'cardActions'
+  | 'left'
+  | 'leftActions'
+  | 'right'
+  | 'icon',
   string
 > = makeStyles(() => ({
   root: {
@@ -34,6 +44,33 @@ const useStyles: () => Record<
     [theme.breakpoints.up('sm')]: {
       width: '480px',
     },
+  },
+  cardActions: {
+    height: '80px',
+    '&.MuiCardActions-root': {
+      padding: '0 30px',
+    },
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+  },
+  left: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  right: {},
+  leftActions: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: '30px',
+  },
+  icon: {
+    paddingRight: '8px',
+    cursor: 'pointer',
   },
 }));
 
