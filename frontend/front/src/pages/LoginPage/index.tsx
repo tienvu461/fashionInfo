9c3aa839ba4ceb13e333296fb46b2aeaffbd6a93
Hai Duc Vu <duchaivu1997@gmail.com>
2021-05-16 22:30:37 +0700

    [FE-Login] update new UI

--- frontend/front/src/pages/LoginPage/index.tsx
+++ frontend/front/src/pages/LoginPage/index.tsx
@@ -13,15 +13,16 @@ import {
   Link,
   InputAdornment,
   IconButton,
+  Divider,
 } from '@material-ui/core';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import AccountCircleOutlined from '@material-ui/icons/AccountCircleOutlined';
 import clsx from 'clsx';
 import useStyles from './useStyles';
 import './_loginpage.scss';
 import { loginAction } from '../../features/Login/LoginAction';
+import iconGg from '../../assets/images/iconfinder_Google_Loginin.png';
+import iconFb from '../../assets/images/iconFb_Login.png';
 
 type FieldStates = {
   username: string;
@@ -63,50 +64,97 @@ function LoginPage(): JSX.Element {
       className={clsx(classes.root && 'login-page')}
       component='main'
       container
+      item
     >
-      <Grid item md={3} sm={12} xs={12} />
+      <Grid item md={6} sm={12} xs={12} className='imageBannerLogin'></Grid>
       <Grid item md={6} sm={12} xs={12}>
         <div className={classes.paper}>
-          <Typography component='h1' variant='h5'>
-            Log in your account!
-          </Typography>
-          <Grid item md={6} sm={8} xs={12}>
+          <Box textAlign='left'>
+            <Typography component='span'>
+              <Box className={classes.header}>Welcome back!</Box>
+            </Typography>
+            <Typography component='span'>
+              <Box className={classes.titleLogin} fontWeight='fontWeightBold'>
+                Đăng nhập
+              </Box>
+            </Typography>
+            <Button
+              startIcon={
+                <Avatar
+                  alt='goole-icon'
+                  src={iconGg}
+                  className={classes.small}
+                />
+              }
+              className={classes.button}
+            >
+              <Typography component='span'>
+                <Box
+                  fontWeight='fontWeightBold'
+                  fontSize={16}
+                  color='#000000'
+                  className={classes.socialButton}
+                >
+                  Qua Google
+                </Box>
+              </Typography>
+            </Button>
+            <Button
+              startIcon={
+                <Avatar
+                  alt='goole-icon'
+                  src={iconFb}
+                  className={classes.small}
+                />
+              }
+              className={classes.button}
+            >
+              <Typography component='span'>
+                <Box
+                  fontWeight='fontWeightBold'
+                  fontSize={16}
+                  color='#000000'
+                  className={classes.socialButton}
+                >
+                  Qua Facebook
+                </Box>
+              </Typography>
+            </Button>
+            {/* <div> */}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: '4px 0 4px 0',
+              }}
+            >
+              <hr className='line'></hr>
+              <span className='title'>hoặc</span>
+              <hr className='line'></hr>
+            </div>
             <form className={classes.form} noValidate onSubmit={handleSubmit}>
+              <div>Email</div>
               <TextField
                 autoComplete='username'
                 autoFocus
                 className={classes.field}
                 fullWidth
                 id='username'
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position='start'>
-                      <Avatar className={classes.avatar}>
-                        <AccountCircleOutlined />
-                      </Avatar>
-                    </InputAdornment>
-                  ),
-                }}
                 margin='normal'
                 name='username'
                 onChange={(event) => handleChange(event)}
-                placeholder='Your Account'
+                placeholder='Email'
                 required
                 variant='outlined'
               />
+              <div style={{ paddingTop: '16px' }}>Mật khẩu</div>
               <TextField
                 autoComplete='password'
                 className={classes.field}
                 fullWidth
                 id='password'
                 InputProps={{
-                  startAdornment: (
-                    <InputAdornment position='start'>
-                      <Avatar className={classes.avatar}>
-                        <LockOutlinedIcon />
-                      </Avatar>
-                    </InputAdornment>
-                  ),
                   endAdornment: (
                     <InputAdornment position='end'>
                       <IconButton
@@ -125,36 +173,43 @@ function LoginPage(): JSX.Element {
                 margin='normal'
                 name='password'
                 onChange={(event) => handleChange(event)}
-                placeholder='Password'
+                placeholder='Mật khẩu'
                 required
                 type={field.showPassword ? 'text' : 'password'}
                 variant='outlined'
               />
-              <FormControlLabel
-                control={<Checkbox color='secondary' value='remember' />}
-                label='Remember me'
-              />
+              <div className={classes.savepassword}>
+                <div>
+                  <FormControlLabel
+                    control={<Checkbox value='remember' color='secondary' />}
+                    label='Ghi nhớ mật khẩu'
+                  />
+                </div>
+                <div>
+                  <Link className={classes.link}>Quên mật khẩu?</Link>
+                </div>
+              </div>
               <Button className={classes.submit} fullWidth type='submit'>
-                Log In
+                <Typography component='span'>
+                  <Box
+                    fontWeight='fontWeightBold'
+                    fontSize={16}
+                    color='#ffffff'
+                    className={classes.socialButton}
+                  >
+                    Đăng nhập
+                  </Box>
+                </Typography>
               </Button>
-              <Grid container>
-                <Grid item md sm xs>
-                  <Link className={classes.link} href='#'>
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item md sm xs>
-                  <Link className={classes.link} href='#'>
-                    Sign Up
-                  </Link>
-                </Grid>
-              </Grid>
             </form>
-          </Grid>
+            <Box textAlign='center'>
+              <span>Chưa có tài khoản?</span>
+              <Link className={classes.link}>Đăng ký ngay</Link>
+            </Box>
+            {/* </div> */}
+          </Box>
         </div>
-        <Box mt={8} />
       </Grid>
-      <Grid item md={3} sm={12} xs={12} />
     </Grid>
   );
 }
--- frontend/front/src/pages/LoginPage/useStyles.ts
+++ frontend/front/src/pages/LoginPage/useStyles.ts
@@ -10,6 +10,12 @@ const useStyles: () => Record<
 | 'form'
 | 'field'
 | 'link'
+| 'small'
+| 'socialButton'
+| 'button'
+| 'savepassword'
+| 'header'
+| 'titleLogin'
 | 'submit', string> = makeStyles((theme: Theme) => ({
   root: {
     width: '100%',
@@ -21,7 +27,8 @@ const useStyles: () => Record<
     margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'left',
+    width: '390px',
   },
   avatar: {
     margin: theme.spacing(1),
@@ -33,7 +40,7 @@ const useStyles: () => Record<
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    backgroundColor: 'black',
+    backgroundColor: '#383838',
     color: 'white',
     '&:hover': {
       backgroundColor: 'gray',
@@ -41,31 +48,58 @@ const useStyles: () => Record<
   },
   field: {
     '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'black',
+      borderColor: 'FAFAFA',
     },
     '&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-      borderColor: '#000',
+      borderColor: '#FAFAFA',
     },
     '& .MuiOutlinedInput-input': {
-      color: 'black',
+      color: 'FAFAFA',
     },
     '&:hover .MuiOutlinedInput-input': {
-      color: 'black',
+      color: 'FAFAFA',
     },
     '& .MuiInputLabel-outlined': {
-      color: 'black',
+      color: 'FAFAFA',
     },
     '&:hover .MuiInputLabel-outlined': {
-      color: '#000',
+      color: '#FAFAFA',
     },
 
     // TextField select
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
     },
+    width: '390px',
+    height: '50px'
   },
   link: {
-    color: '#000',
+    color: '#2C5282',
+  },
+  small: {
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+  },
+  socialButton: {
+    textTransform: 'none'
+  },
+  button: {
+    width: '175px',
+    height: '50px'
+  },
+  savepassword: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: '16px',
+    color: '#2D3748',
+  },
+  titleLogin: {
+    fontSize: '30px',
+    color: '#2D3748',
+    paddingBottom: '33px',
   }
 }));
 
