bd711719b079203e0fbd0d9ab955ddbcec864fad
Hai Vu <duchaivu1997@gmail.com>
2021-05-22 20:47:33 +0700

    [feature_search] handle input text

--- frontend/front/src/components/HeaderMenu/components/Search/index.tsx
+++ frontend/front/src/components/HeaderMenu/components/Search/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid, InputBase } from '@material-ui/core';
 import searchIcon from '../../../../assets/images/searchIcon.svg';
 
@@ -6,6 +6,17 @@ import useStyles from './useStyles';
 
 function Search(): JSX.Element {
   const classes = useStyles();
+  const [value, setValue] = useState();
+
+  const onChange = (event) => { setValue(event.target.value) };
+
+  const handleKeyDown = (event) => {
+    if (event.target.value !== '') {
+      if (event.key === 'Enter') {
+      console.log('Enter', event.target.value)
+    }
+    }
+  }
 
   return (
     <Grid className={classes.search}>
@@ -18,6 +29,10 @@ function Search(): JSX.Element {
           input: classes.inputInput,
         }}
         inputProps={{ 'aria-label': 'search' }}
+        name='textSearch'
+        placeholder='Search...'
+        onChange={onChange}
+        onKeyDown={handleKeyDown}
       />
     </Grid>
   );
