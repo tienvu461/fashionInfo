e2f823b9b8f60e18fbad6952f9eedbf6fdd40ebb
Chau Nguyen <c.chau.nguyen@zonarsystems.net>
2021-05-26 21:28:50 +0700

    [fe_login] Encode JWT

--- frontend/front/src/features/Login/LoginAction.ts
+++ frontend/front/src/features/Login/LoginAction.ts
@@ -1,4 +1,6 @@
+/* eslint-disable camelcase */
 import { Dispatch } from '@reduxjs/toolkit';
+import jwtDecode, { JwtPayload } from "jwt-decode";
 import { loginSucess, loginFail } from './LoginSlice';
 import { loginService, getUrlSocialService } from '../../services/auth';
 import { setDataFromLocalStorage, setTokenToLocalStorage } from '../../utils/localStorage';
@@ -6,18 +8,21 @@ import { setDataFromLocalStorage, setTokenToLocalStorage } from '../../utils/loc
 export const loginAction = (payload: unknown) => async (dispatch: Dispatch) => {
         try {
             const response = await loginService(payload);
-            // console.log('LOGIN SUCCESS', response);
+            console.log('Data login success', response);
+            /**
+             * TO DO ENOCODE JWT
+             */
+            const dataEncodeJwt = jwtDecode<any>(response.data.access);
+            const { user_id } = dataEncodeJwt;
             const { data = {}, status = '' } = response;
             if (status === 200) {
-                dispatch(loginSucess({ data, status }));
+                dispatch(loginSucess({ data, status, user_id }));
                 setDataFromLocalStorage(JSON.stringify(response));
                 setTokenToLocalStorage(data.access);
-                // console.log("RES", response);
             }
         } catch (error) {
             const { response: { data = {}, status = '' } = {}, } = error;
             dispatch(loginFail({ data, status }));
-            // console.log('LOGIN ERROR', error);
         }
     };
 
