945d90b74a0e36a580607c9df4da89830ac831bd
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-05-27 16:44:33 +0700

    Add Text input comments

--- frontend/front/src/pages/PhotoPage/components/Photos/Detail/components/CommentSection/components/Comments/index.tsx
+++ frontend/front/src/pages/PhotoPage/components/Photos/Detail/components/CommentSection/components/Comments/index.tsx
@@ -1,19 +1,28 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-unresolved */
-import React, { useMemo } from 'react';
-import { Grid } from '@material-ui/core';
-import { Timeline } from '@material-ui/lab';
+import React, { useMemo, useState, useRef } from 'react';
+import { Grid, Avatar, Paper, Typography, TextField } from '@material-ui/core';
+import {
+  Timeline,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineSeparator,
+} from '@material-ui/lab';
 import { isEmpty } from 'lodash';
 import { useSelector } from 'react-redux';
 import { RootState } from 'src/store/store';
 import Ava1 from 'src/assets/images/menAva.jpg';
+import Ava3 from 'src/assets/images/ava3.svg';
 
 import useStyles from './useStyles';
 import CommentParrent from './CommentParrent';
 
 function Comments(): JSX.Element {
   const classes = useStyles();
+  const [textArea, setTextArea] = useState<string>('');
+  const valueRef = useRef<HTMLInputElement>();
   const comments = useSelector((state: RootState) => state.photo.photoDetail.comments);
 
   const listComments = useMemo(
@@ -33,9 +42,50 @@ function Comments(): JSX.Element {
     ), [comments]
   );
 
+  const onTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextArea(e.target.value);
+  };
+
+  const onKeyPress = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      console.log(textArea);
+      valueRef.current?.blur();
+    }
+  };
+
   return (
     <Grid className={classes.root}>
       <Timeline className={classes.rootTimeline}>{listComments}</Timeline>
+      <Timeline className={classes.rootTimeline}>
+        <TimelineItem className={classes.timeline}>
+          <TimelineSeparator>
+            <TimelineDot className={classes.dotAvatar}>
+              <Avatar alt='ava' className={classes.avatar} src={Ava3} />
+            </TimelineDot>
+          </TimelineSeparator>
+          <TimelineContent className={classes.content}>
+            <Paper className={classes.paper} elevation={3}>
+              <Typography className={`${classes.actionName} ${classes.textStyle}`} component='h6' variant='h6'>
+                Anh Ngoc Dang Nguyen
+              </Typography>
+              <TextField
+                className={classes.textArea}
+                multiline
+                rows={1}
+                rowsMax={4}
+                aria-label='maximum height'
+                placeholder='Viết bình luận...'
+                InputProps={{
+                  classes: { input: classes.inputTextArea },
+                }}
+                onChange={onTextFieldChange}
+                onKeyPress={onKeyPress}
+                inputRef={valueRef}
+              />
+            </Paper>
+          </TimelineContent>
+        </TimelineItem>
+      </Timeline>
     </Grid>
   );
 }
