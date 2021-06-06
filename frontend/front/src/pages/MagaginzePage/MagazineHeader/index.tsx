bf2d0184963448d9a013e7ada3b86d02338f126e
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-06-06 20:58:07 +0700

    Add banner and fix responsive

--- frontend/front/src/pages/MagaginzePage/MagazineHeader/index.tsx
+++ frontend/front/src/pages/MagaginzePage/MagazineHeader/index.tsx
@@ -1,9 +1,18 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
+import { Grid } from '@material-ui/core';
+import banner from 'src/assets/images/magazine/banner.png';
+
+import './_magazineHeader.scss';
 
 function MagazineHeader(): JSX.Element {
     return (
-      <div>
-        Header
+      <div className='magazineHeader'>
+        <Grid container>
+          <Grid className='banner' item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <img className='banner-img' src={banner} alt='banner' />
+          </Grid>
+        </Grid>
       </div>
     );
 }
