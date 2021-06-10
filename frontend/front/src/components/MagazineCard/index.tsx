5f1e8711e3b541744af2f8dbcaa6ff4192cc5576
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-06-10 23:41:58 +0700

    Fix bug render content

--- frontend/front/src/components/MagazineCard/index.tsx
+++ frontend/front/src/components/MagazineCard/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import { Card, CardActionArea, CardContent, CardMedia, Divider, Typography } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, CardMedia, Divider, Typography, useMediaQuery } from '@material-ui/core';
 import moment from 'moment';
 
 import cardImg from 'src/assets/images/magazine/magazineCard.png';
@@ -23,8 +23,22 @@ function MagazineCard(props: MagazineCardProps): JSX.Element {
     created_at: createAt = '',
     thumbnail = '',
   } = cardProps;
+  const screenMin = useMediaQuery('(min-width:1280px)');
+  const screenMax = useMediaQuery('(max-width:1920px)');
 
   const formatDate = (time: number) => moment(time * 1000).fromNow();
+  const formatText = (text: string, key: string) => {
+    if (key === 'title') {
+      if (screenMin && screenMax && text.length >= 30) {
+        const newText = text.replace(/^(.{40}[^\s]*).*/, '$1');
+        return `${newText}...`;
+      }
+    } else if (screenMin && screenMax && text.length >= 30) {
+        const newText = text.replace(/^(.{60}[^\s]*).*/, '$1');
+        return `${newText}...`;
+      }
+    return text;
+  };
 
     return (
       <>
@@ -41,7 +55,7 @@ function MagazineCard(props: MagazineCardProps): JSX.Element {
                   className={`${classes.headerText} ${classes.subCategory}`}
                   gutterBottom
                   variant='h6'
-                  component='h2'
+                  component='h6'
                 >
                   {subCategory}
                 </Typography>
@@ -50,16 +64,18 @@ function MagazineCard(props: MagazineCardProps): JSX.Element {
                   className={`${classes.headerText} ${classes.time}`}
                   gutterBottom
                   variant='h6'
-                  component='h2'
+                  component='h6'
                 >
                   {formatDate(createAt)}
                 </Typography>
               </div>
-              <Typography className={classes.titleCard} variant='h4' color='textSecondary' component='h4'>
-                {title}
+              <Typography className={`${classes.titleCard} titleCard`} variant='h4' color='textSecondary' component='h4'>
+                {formatText(title, 'title')}
+                {/* {title} */}
               </Typography>
-              <Typography className={classes.summary} variant='h6' color='textSecondary' component='h6'>
-                {summary}
+              <Typography className={`${classes.summary} summary`} variant='h6' color='textSecondary' component='h6'>
+                {formatText(summary, 'summary')}
+                {/* {summary} */}
               </Typography>
             </CardContent>
           </CardActionArea>
