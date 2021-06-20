296390c31d3df2310635f9cafa6e6d65e016f52b
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-06-20 12:26:00 +0700

    Refactore code search input and add the clear icon and loading icon

--- frontend/front/src/components/HeaderMenu/components/Search/index.tsx
+++ frontend/front/src/components/HeaderMenu/components/Search/index.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-unresolved */
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { CircularProgress, Grid, InputBase } from '@material-ui/core';
+import { CircularProgress, Grid, IconButton, InputBase, TextField, InputAdornment } from '@material-ui/core';
 import { debounce } from 'lodash';
 
 import searchIcon from 'src/assets/images/searchIcon.svg';
+import clearIcon from 'src/assets/images/clearIcon.png';
 import { searchAction } from 'src/features/Search/searchAction';
 import useStyles from './useStyles';
 
@@ -14,6 +15,9 @@ function Search(): JSX.Element {
   const history = useHistory();
   const [value, setValue] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+  const [click, setClick] = useState<boolean>(false);
+  const [clear, setClear] = useState<boolean>(false);
+  const [focused, setFocused] = useState(false);
   const valueRef = useRef<HTMLInputElement>();
   const dispatch = useDispatch();
   const onChange = (event) => {
@@ -38,35 +42,94 @@ function Search(): JSX.Element {
     }
   };
 
-  const loadingSearch = () => (
+  const loadingIcon = () => (
     <>
-      {
-        loading ? <CircularProgress size={20} /> : null
-      }
+      {loading ? (
+        <CircularProgress size={20} /> // loading icon
+      ) : (
+        <>
+          {click ? (
+            <img
+              alt='clear-search'
+              onMouseEnter={() => onMouse('hover')}
+              onMouseLeave={() => onMouse('leave')}
+              src={clearIcon}
+              onClick={(e) => clearSearch(e)}
+              style={{ cursor: 'pointer' }}
+            />
+          ) : null}
+        </>
+      )}
     </>
   );
 
+  const triggerClick = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    setClick(true); // [1] open TEXT FIELD, so function onFocus() is called
+  };
+
+  const onFocus = () => {
+    // focused = true in order to hold the input always open, so useEffect does not run
+    setFocused(true);
+  };
+
+  const onMouse = (key: string) => {
+    if (key === 'hover') { // [2.1]: USER HOVER A X ICON, THEN SET STATE TO HOLDING THE INPUT ALWAYS OPEN. AND THE useEffect does not run.
+      setClear(true);
+    } else { // [2.2]: USER LEAVE THE MOUSE POINTER OUT OF THE X ICON, SET STATE CLEAR = FALSE TO RUN THE FUNCTION onBlur().
+      setClear(false);
+    }
+  };
+
+  const onBlur = () => {
+    if (clear) { // [2.1] IF USER HOVER A X ICON (means state clear = true), THEN CLICK X ICON WHILE HOLDING THE INPUT IS OPENING (focused = true)
+      setFocused(true);
+    } else { // [2.2] IF NOT, USER CLICK ANYWHERE TO CLOSE THE INPUT. The useEffect will see the focused = false so it runs to close the input.
+      setFocused(false);
+    }
+  };
+
+  const clearSearch = (e) => {
+    e.preventDefault();
+    // [3] clear text
+    console.log('clear');
+  };
+
+  useEffect(() => {
+    if (!focused) {
+      setClick(false);
+    }
+  }, [focused]);
+
   return (
-    <Grid className={classes.search}>
-      <Grid className={classes.searchIcon}>
-        <img alt='Search' src={searchIcon} />
+    <>
+      <Grid className={classes.search}>
+        <Grid container spacing={1} alignItems='flex-end'>
+          <Grid item className={classes.searchIcon}>
+            <img alt='Search' src={searchIcon} />
+          </Grid>
+          <Grid item>
+            <TextField
+              onBlur={onBlur}
+              onFocus={onFocus}
+              onClick={(e) => triggerClick(e)}
+              onChange={onChange}
+              value={value}
+              inputRef={valueRef}
+              onKeyDown={handleKeyDown}
+              style={{ fontFamily: 'Roboto', fontSize: '24' }}
+              InputProps={{
+                className: focused ? classes.inputInput : classes.not,
+                classes: { input: classes.placeHolderInput },
+                endAdornment: loadingIcon(),
+              }}
+              id='input-with-icon-grid'
+              placeholder='Search...'
+            />
+          </Grid>
+        </Grid>
       </Grid>
-      <InputBase
-        classes={{
-          root: classes.inputRoot,
-          input: classes.inputInput,
-        }}
-        inputProps={{ 'aria-label': 'search' }}
-        name='textSearch'
-        placeholder='Search...'
-        onChange={onChange}
-        value={value}
-        inputRef={valueRef}
-        onKeyDown={handleKeyDown}
-        style={{ fontFamily: 'Roboto', fontSize: '24' }}
-        endAdornment={loadingSearch()}
-      />
-    </Grid>
+    </>
   );
 }
 
--- frontend/front/src/components/HeaderMenu/components/Search/useStyles.ts
+++ frontend/front/src/components/HeaderMenu/components/Search/useStyles.ts
@@ -1,9 +1,9 @@
 import { makeStyles, Theme, fade } from '@material-ui/core/styles';
 
-const useStyles: () => Record<'search' | 'searchIcon' | 'inputRoot' | 'inputInput', string> = makeStyles(
+const useStyles: () => Record<'search' | 'not' | 'placeHolderInput' | 'searchIcon' | 'inputInput', string> = makeStyles(
   (theme: Theme) => ({
     search: {
-      position: 'relative',
+      // position: 'relative',
       borderRadius: theme.shape.borderRadius,
       backgroundColor: fade(theme.palette.common.white, 0.15),
       '&:hover': { backgroundColor: fade(theme.palette.common.white, 0.25) },
@@ -20,23 +20,57 @@ const useStyles: () => Record<'search' | 'searchIcon' | 'inputRoot' | 'inputInpu
     searchIcon: {
       height: '100%',
       position: 'absolute',
-      pointerEvents: 'none',
       display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
+      alignItems: 'flex-end',
+      marginBottom: '5px',
+      pointerEvents: 'none',
       color: '#474747',
     },
-    inputRoot: { color: 'inherit' },
     inputInput: {
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
+      paddingLeft: '30px',
+      width: '50vh',
       transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '0ch',
-        '&:focus': { width: '20ch' },
+      '&.Mui-focused': {
+        width: '50vh',
+      },
+      '&.MuiInput-underline:before': {
+        display: 'none',
       },
     },
+    not: {
+      position: 'relative',
+      width: 0,
+      paddingLeft: '30px',
+      transition: theme.transitions.create('width'),
+      '&.MuiInput-underline:before': {
+        display: 'none',
+      },
+    },
+    placeHolderInput: {
+      '&::placeholder': {
+        fontSize: '24px',
+      },
+    },
+    // searchIcon: {
+    //   height: '100%',
+    //   position: 'absolute',
+    //   pointerEvents: 'none',
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    //   color: '#474747',
+    // },
+    // inputRoot: { color: 'inherit' },
+    // inputInput: {
+    //   // vertical padding + font size from searchIcon
+    //   paddingLeft: `calc(1em + ${theme.spacing(3)}px)`,
+    //   transition: theme.transitions.create('width'),
+    //   width: '100%',
+    //   [theme.breakpoints.up('sm')]: {
+    //     width: '0ch',
+    //     '&:focus': { width: '20ch' },
+    //   },
+    // },
   })
 );
 
