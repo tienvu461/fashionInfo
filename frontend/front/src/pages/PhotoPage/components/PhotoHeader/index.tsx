9729a47a11f37647d3ae382a91b1f0c4eee4061c
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-06-22 22:28:14 +0700

    Update feature photo and handle clickable

--- frontend/front/src/pages/PhotoPage/components/PhotoHeader/index.tsx
+++ frontend/front/src/pages/PhotoPage/components/PhotoHeader/index.tsx
@@ -1,23 +1,27 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Typography, useMediaQuery } from '@material-ui/core';
 import BannerPic from 'src/assets/images/photos/hotPic.jpg';
 import { RootState } from 'src/store/store';
 import { HOST } from 'src/apis';
+import { ROUTE_PHOTO } from 'src/constants';
 
 import useStyles from './useStyles';
 import './_photoHeader.scss';
 
 function PhotoHeader(): JSX.Element {
   const classes = useStyles();
+  const history = useHistory();
   const matches = useMediaQuery('(max-width:600px)');
   const matches1 = useMediaQuery('(min-width:1600px)');
   const matches2 = useMediaQuery('(min-width:960px)');
   const matches3 = useMediaQuery('(max-width:960px)');
 
   const featurePhoto = useSelector((state: RootState) => state.featurePhoto.featureList);
+  const id = featurePhoto[0]?.feature_photo.photo_id;
 
   const bannerInfo: {
     headTitle: string;
@@ -72,11 +76,15 @@ function PhotoHeader(): JSX.Element {
             {bannerInfo.headTitle}
           </Typography>
         </div>
-        <div className='right-section' style={matches ? { marginBottom: '80px' } : {}}>
+        <div
+          onClick={() => history.push(`${ROUTE_PHOTO}/${id}`)}
+          className='right-section'
+          style={matches ? { marginBottom: '80px' } : {}}
+        >
           <LazyLoadImage
             alt='Feature Photo'
             className={classes.imgBanner}
-            src={checkPathImg(featurePhoto[0]?.feature_photo)}
+            src={checkPathImg(featurePhoto[0]?.feature_photo.image_path)}
             effect='blur'
             height={controlHeithImg()}
             width={controlWidthImg()}
