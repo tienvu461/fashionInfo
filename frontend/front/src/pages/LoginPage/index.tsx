4407bc142dd50be9578e7405a8fb58af7db75d1f
Chau Nguyen <c.chau.nguyen@zonarsystems.net>
2021-06-22 16:49:35 +0700

    [Fix-bug] responsive login pop up, register, login page

--- frontend/front/src/pages/LoginPage/index.tsx
+++ frontend/front/src/pages/LoginPage/index.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable import/no-unresolved */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import clsx from 'clsx';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import Link from 'react-router-dom/Link';
 import {
   Typography,
@@ -15,16 +16,17 @@ import {
   FormControlLabel,
   Checkbox,
   CircularProgress,
+  IconButton,
 } from '@material-ui/core';
-
+import CloseIcon from '@material-ui/icons/Close';
 import { RootState } from 'src/store/store';
 import { getUserProfile } from 'src/features/Profile/ProfileAction';
 import { loginAction, getUrlSocialAction } from 'src/features/Login/LoginAction';
 
+import imgLogin from 'src/assets/images/Mask_Group_Login.png';
 import iconFb from 'src/assets/images/iconFb_Login.png';
 import iconGg from 'src/assets/images/iconfinder_Google_Loginin.png';
-import { ROUTE_HOME } from 'src/constants';
-
+import { ROUTE_HOME, ROUTE_LOGIN } from 'src/constants';
 import './_loginpage.scss';
 import useStyles from './useStyles';
 
@@ -33,19 +35,30 @@ type FieldStates = {
   password: string;
 };
 
-function LoginPage(): JSX.Element {
+function LoginPage({ closePopup }): JSX.Element {
   const classes = useStyles();
   const dispatch = useDispatch<any>();
   const history = useHistory();
+  const location = useLocation();
 
   const [loading, setLoading] = useState<boolean>(false);
   const [field, setfield] = useState<FieldStates>({
     email: '',
     password: '',
   });
+  const [popUp, setPopUp] = useState<boolean>(false);
   const loginStatus = useSelector((state: RootState) => state.login.loginResponse.status);
   const cmtPhotoId = useSelector((state: RootState) => state.login.isLoginToComment.paramId);
   const keyToRedirect = useSelector((state: RootState) => state.login.isLoginToComment.key);
+  // Prevent re-login when login successful
+  useEffect(() => {
+    if (loginStatus === 200) {
+      history.push(`${ROUTE_HOME}`);
+    }
+    if (!(location.pathname === ROUTE_LOGIN)) {
+      setPopUp(true)
+    }
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setfield({
@@ -77,130 +90,173 @@ function LoginPage(): JSX.Element {
 
   function handleError() {
     if (loginStatus === 400) {
-      return <span className={classes.errorText}>Vui lòng nhập tài khoản/email và mật khẩu</span>;
+      return <span className={classes.errorText}>Vui lòng nhập Email và mật khẩu</span>;
     }
     if (loginStatus === 401 || loginStatus === 500) {
-      return <span className={classes.errorText}>Tài khoản/Email hoặc mật khẩu không đúng</span>;
+      return <span className={classes.errorText}>Email hoặc mật khẩu không đúng</span>;
     }
     return null;
   }
 
+  const handleClose = () => {
+    closePopup(false);
+  };
+
+  function handleCloseLoginPopup() {
+    if (!(location.pathname === ROUTE_LOGIN)) {
+      return (
+        <IconButton aria-label='close' style={{ position: 'absolute', right: 0 }} onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      );
+    }
+    return null;
+  }
+
+  const loginInfo: {
+    image: string;
+  } = {
+    image: imgLogin,
+  }
+
   return (
-    <Grid className={clsx(classes.root && 'login-page')} component='main' container item>
-      <Grid className='imageBannerLogin' item md={6} sm={12} xs={12} />
-      <Grid item md={6} sm={12} xs={12}>
-        <div className={clsx(classes.paper && 'paper')}>
-          <Box textAlign='left'>
-            <Typography component='span'>
-              <Box className={classes.header}>Welcome back!</Box>
-            </Typography>
-            <Typography component='span'>
-              <Box className={classes.titleLogin} fontWeight='fontWeightBold'>
-                Đăng nhập
-              </Box>
-            </Typography>
-            <div className={classes.savepassword}>
-              <Button
-                className={classes.button}
-                onClick={() => {
-                  dispatch(getUrlSocialAction());
-                }}
-                startIcon={<Avatar alt='goole-icon' className={classes.small} src={iconGg} />}
-              >
-                <Typography component='span'>
-                  <Box className={classes.socialButton} color='#000000' fontSize={16} fontWeight='fontWeightBold'>
-                    Qua Google
-                  </Box>
-                </Typography>
-              </Button>
-              <Button
-                className={classes.button}
-                startIcon={<Avatar alt='goole-icon' className={classes.small} src={iconFb} />}
-              >
-                <Typography component='span'>
-                  <Box className={classes.socialButton} color='#000000' fontSize={16} fontWeight='fontWeightBold'>
-                    Qua Facebook
-                  </Box>
-                </Typography>
-              </Button>
-            </div>
-            {/* <div> */}
-            <div
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                padding: '4px 0 4px 0',
-              }}
-            >
-              <hr className='line' />
-              <span className={classes.title}>hoặc</span>
-              <hr className='line' />
-            </div>
-            <form className={classes.form} noValidate onSubmit={handleSubmit}>
-              <div className={classes.fontManual}>Email</div>
-              <TextField
-                autoComplete='email'
-                autoFocus
-                className={classes.field}
-                fullWidth
-                id='email'
-                margin='normal'
-                name='email'
-                onChange={(event) => handleChange(event)}
-                required
-                variant='outlined'
-              />
-              <div className={classes.fontManual} style={{ paddingTop: '16px' }}>
-                Mật khẩu
-              </div>
-              <TextField
-                autoComplete='password'
-                className={classes.field}
-                fullWidth
-                id='password'
-                margin='normal'
-                name='password'
-                onChange={(event) => handleChange(event)}
-                required
-                type='password'
-                variant='outlined'
-              />
-              <Typography>{handleError()}</Typography>
-              <div className={classes.savepassword}>
-                <div>
-                  <FormControlLabel
-                    control={<Checkbox color='secondary' value='remember' />}
-                    label={<span style={{ fontFamily: 'Roboto', fontSize: '14' }}>Ghi nhớ mật khẩu</span>}
-                  />
-                </div>
-                <div>
-                  <Link className={classes.link}>
-                    <span className={classes.fontManual}>Quên mật khẩu?</span>
-                  </Link>
-                </div>
-              </div>
-              <Button
-                className={classes.submit}
-                fullWidth
-                type='submit'
-              >
-                {loading ? (
-                  <CircularProgress />
-                ) : (
-                  <Typography component='h6' className={classes.socialButton}>
-                    Đăng nhập
-                  </Typography>
-                )}
-              </Button>
-            </form>
-            <Box textAlign='center'>
-              <span className={classes.fontManual}>Chưa có tài khoản?</span>
-              <Link className={classes.link} to='/register'>
-                <span className={classes.fontManual} style={{ cursor: 'pointer' }}> Đăng ký ngay</span>
-              </Link>
+    <Grid className={classes.root} container>
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={6}
+        lg={6}
+        xl={6}
+        className={classes.leftLoginPage}
+      >
+        <img
+          alt='login'
+          className={classes.loginImage}
+          src={loginInfo.image}
+        />
+      </Grid>
+      {handleCloseLoginPopup()}
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={6}
+        lg={6}
+        xl={6}
+        className={popUp ? classes.popupLogin : classes.rightLoginPage}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <div className={classes.setWidthField}>
+          <Typography component='span'>
+            <Box className={classes.header}>Welcome back!</Box>
+          </Typography>
+          <Typography component='span'>
+            <Box className={classes.titleLogin} fontWeight='fontWeightBold'>
+              Đăng nhập
             </Box>
+          </Typography>
+          <div className={classes.savepassword}>
+            <Button
+              className={classes.button}
+              onClick={() => {
+                dispatch(getUrlSocialAction());
+              }}
+              startIcon={<Avatar alt='goole-icon' className={classes.small} src={iconGg} />}
+            >
+              <Typography component='span'>
+                <Box className={classes.socialButton} color='#000000' fontSize={16} fontWeight='fontWeightBold'>
+                  Qua Google
+                </Box>
+              </Typography>
+            </Button>
+            <Button
+              className={classes.button}
+              startIcon={<Avatar alt='goole-icon' className={classes.small} src={iconFb} />}
+            >
+              <Typography component='span'>
+                <Box className={classes.socialButton} color='#000000' fontSize={16} fontWeight='fontWeightBold'>
+                  Qua Facebook
+                </Box>
+              </Typography>
+            </Button>
+          </div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: '4px 0 4px 0',
+            }}
+          >
+            <hr className={classes.line} />
+            <span className={classes.title}>hoặc</span>
+            <hr className={classes.line} />
+          </div>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <div className={classes.fontManual}>Email</div>
+            <TextField
+              autoComplete='email'
+              // autoFocus
+              className={classes.field}
+              fullWidth
+              id='email'
+              margin='normal'
+              name='email'
+              onChange={(event) => handleChange(event)}
+              required
+              variant='outlined'
+            />
+            <div className={classes.fontManual} style={{ paddingTop: '16px' }}>
+              Mật khẩu
+            </div>
+            <TextField
+              autoComplete='password'
+              className={classes.field}
+              fullWidth
+              id='password'
+              margin='normal'
+              name='password'
+              onChange={(event) => handleChange(event)}
+              required
+              type='password'
+              variant='outlined'
+            />
+            <Typography>{handleError()}</Typography>
+            <div className={classes.savepassword}>
+              <div>
+                <FormControlLabel
+                  control={<Checkbox color='secondary' value='remember' />}
+                  label={<span style={{ fontFamily: 'Roboto', fontSize: '14' }}>Ghi nhớ mật khẩu</span>}
+                />
+              </div>
+              <div>
+                <Link className={classes.link}>
+                  <span className={classes.fontManual}>Quên mật khẩu?</span>
+                </Link>
+              </div>
+            </div>
+            <Button
+              className={classes.submit}
+              fullWidth
+              type='submit'
+            >
+              {loading ? (
+                <CircularProgress />
+              ) : (
+                <Typography component='h6' className={classes.socialButton}>
+                  Đăng nhập
+                </Typography>
+              )}
+            </Button>
+          </form>
+          <Box textAlign='center'>
+            <span className={classes.fontManual}>Chưa có tài khoản?</span>
+            <Link className={classes.link} to='/register'>
+              <span className={classes.fontManual} style={{ cursor: 'pointer' }}> Đăng ký ngay</span>
+            </Link>
           </Box>
+
         </div>
       </Grid>
     </Grid>
--- frontend/front/src/pages/LoginPage/useStyles.ts
+++ frontend/front/src/pages/LoginPage/useStyles.ts
@@ -1,3 +1,4 @@
+import { green, red, blue, pink, orange } from '@material-ui/core/colors';
 import {
   makeStyles,
   Theme,
@@ -5,54 +6,99 @@ import {
 
 const useStyles: () => Record<
   | 'root'
-  | 'paper'
-  | 'avatar'
-  | 'form'
-  | 'field'
-  | 'link'
+  | 'leftLoginPage'
+  | 'loginImage'
+  | 'header'
+  | 'titleLogin'
+  | 'savepassword'
+  | 'button'
   | 'small'
   | 'socialButton'
-  | 'button'
-  | 'savepassword'
-  | 'header'
-  | 'fontManual'
-  | 'titleLogin'
   | 'title'
+  | 'fontManual'
+  | 'field'
+  | 'link'
+  | 'form'
+  | 'submit'
   | 'errorText'
-  | 'submit',
+  | 'rightLoginPage'
+  | 'line'
+  | 'popupLogin'
+  | 'setWidthField',
   string
 > = makeStyles((theme: Theme) => ({
   root: {
     width: '100%',
-    height: '90vh',
-    display: 'flex',
-    flexDirection: 'row',
+    height: 'auto',
   },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'left',
-    width: '390px',
+  leftLoginPage: {
+    height: 'fit-content',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none'
+    }
   },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+  loginImage: {
+    width: '100%',
+  },
+  header: {
+    fontSize: '16px',
+    color: '#2D3748',
+    fontFamily: 'Roboto',
+    textAlign: 'left',
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '10px',
+    }
+  },
+  titleLogin: {
+    fontSize: '30px',
+    color: '#2D3748',
+    paddingBottom: '33px',
+    fontFamily: 'Roboto',
+    textAlign: 'left',
+    [theme.breakpoints.down('sm')]: {
+      paddingLeft: '10px',
+    }
+  },
+  savepassword: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0px 10px',
+    }
+  },
+  button: {
+    width: '175px',
+    height: '50px',
+    backgroundColor: '#FFFFFF',
+    [theme.breakpoints.down('sm')]: {
+      width: '155px',
+    }
+  },
+  small: {
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+  },
+  socialButton: {
+    textTransform: 'none',
+    fontFamily: 'Roboto',
+    fontSize: '18px',
+  },
+  title: {
+    paddingRight: '20px',
+    paddingLeft: '20px',
+    fontFamily: 'Roboto',
   },
   form: {
     width: '100%',
     marginTop: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+      padding: '0px 10px',
+    }
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    backgroundColor: '#383838',
-    color: 'white',
-    '&:hover': {
-      backgroundColor: 'gray',
-    },
-    '&.MuiButton-root': {
-      padding: '15px 0px',
-    },
+  fontManual: {
+    fontFamily: 'Roboto',
+    textAlign: 'left',
   },
   field: {
     '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
@@ -79,54 +125,96 @@ const useStyles: () => Record<
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
     },
-    width: '390px',
+    width: '100%',
     height: '50px',
   },
   link: {
     color: '#2C5282',
   },
-  small: {
-    width: theme.spacing(2),
-    height: theme.spacing(2),
-  },
-  socialButton: {
-    textTransform: 'none',
-    fontFamily: 'Roboto',
-    fontSize: '18px',
-  },
-  button: {
-    width: '175px',
-    height: '50px',
-    backgroundColor: '#FFFFFF',
-  },
-  savepassword: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  header: {
-    fontSize: '16px',
-    color: '#2D3748',
-    fontFamily: 'Roboto',
-  },
-  titleLogin: {
-    fontSize: '30px',
-    color: '#2D3748',
-    paddingBottom: '33px',
-    fontFamily: 'Roboto',
-  },
-  fontManual: {
-    fontFamily: 'Roboto',
-  },
-  title: {
-    paddingRight: '20px',
-    paddingLeft: '20px',
-    fontFamily: 'Roboto',
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    backgroundColor: '#383838',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'gray',
+    },
+    '&.MuiButton-root': {
+      padding: '15px 0px',
+    },
   },
   errorText: {
     fontFamily: 'Roboto',
     color: '#FF0000',
   },
+  rightLoginPage: {
+    [theme.breakpoints.up('xs')]: {
+      // background: green[500],
+      // border: '10px solid #fafafa',
+      paddingTop: '40px'
+    },
+    [theme.breakpoints.up('sm')]: {
+      // background: pink[500],
+      border: '10px solid #fafafa',
+      paddingTop: '40px'
+    },
+    [theme.breakpoints.up('md')]: {
+      // background: red[500],
+      paddingTop: '40px'
+    },
+    [theme.breakpoints.up('lg')]: {
+      // background: blue[500],
+      paddingTop: '40px',
+    },
+    [theme.breakpoints.up('xl')]: {
+      // background: orange[500],
+      paddingTop: '80px',
+    }
+  },
+  popupLogin: {
+    paddingTop: '40px',
+  },
+  setWidthField: {
+    [theme.breakpoints.up('xs')]: {
+      width: '400px',
+    },
+    [theme.breakpoints.up('sm')]: {
+      // width: '400px',
+    },
+    [theme.breakpoints.up('md')]: {
+      // width: '400px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      // width: '400px',
+    },
+    [theme.breakpoints.up('xl')]: {
+      // width: '400px',
+    }
+  },
+  line: {
+    height: '1px',
+    width: '52px',
+    // border- width: 0;
+    // color: 'gray';
+    // backgroundColor: 'gray';
+    margin: '0px',
+  }
+  // root: {
+  //   width: '100%',
+  //   height: '90vh',
+  //   display: 'flex',
+  //   flexDirection: 'row',
+  // },
+  // paper: {
+  //   margin: theme.spacing(8, 4),
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   alignItems: 'left',
+  //   width: '390px',
+  // },
+  // avatar: {
+  //   margin: theme.spacing(1),
+  //   backgroundColor: theme.palette.secondary.main,
+  // },
 }));
 
 export { useStyles as default };
