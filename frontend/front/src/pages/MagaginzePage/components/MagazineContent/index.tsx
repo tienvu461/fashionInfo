564f5e0d62981291a885c20de5b1743767b96943
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-07-02 20:52:22 +0700

    Fix effective for loading image

--- frontend/front/src/pages/MagaginzePage/components/MagazineContent/index.tsx
+++ frontend/front/src/pages/MagaginzePage/components/MagazineContent/index.tsx
@@ -5,10 +5,9 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { CircularProgress, Divider, Grid, RootRef, Typography, useMediaQuery } from '@material-ui/core';
+import { CircularProgress, Divider, Grid, RootRef, Typography } from '@material-ui/core';
 import { isEmpty } from 'lodash';
 import moment from 'moment';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 import MagazineCard from 'src/components/MagazineCard';
 import { getListMagazineAction } from 'src/features/Magazine/MagazineAction';
@@ -22,11 +21,12 @@ import './_magazine.scss';
 
 interface MangazineContentProps {
   category: string;
+  loading: boolean;
 }
 
 const MagazineContent: React.FunctionComponent<MangazineContentProps> = (props) => {
   const classes = useStyles();
-  const { category = '' } = props;
+  const { category = '', loading: loadingTab } = props;
   const dispatch = useDispatch<any>();
   const history = useHistory();
 
@@ -39,10 +39,6 @@ const MagazineContent: React.FunctionComponent<MangazineContentProps> = (props) 
   const featureListMagazine = useSelector((state: RootState) => state.featurePhoto.featureListMagazine);
 
   const formatDate = (time: number) => moment(time * 1000).fromNow();
-
-  const matches = useMediaQuery('(min-width:1600px)');
-  const matches1 = useMediaQuery('(min-width:1280px)');
-  const matches2 = useMediaQuery('(min-width:960px)');
 
   const checkPathImg = (path) => {
     if (path.includes(HOST)) {
@@ -99,30 +95,6 @@ const MagazineContent: React.FunctionComponent<MangazineContentProps> = (props) 
     valueRef?.current?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
   };
 
-   const controlWidthImg = () => {
-     if (matches) {
-       return 740;
-     }
-     if (matches1) {
-       return 1160 * (37 / 76);
-     }
-     if (matches2) return '100%';
-
-     return '100%';
-   };
-
-   const controlHeithImg = () => {
-     if (matches) {
-       return 600;
-     }
-     if (matches1) {
-       return 1160 * (15 / 38);
-     }
-     if (matches2) return 'auto';
-
-     return '100%';
-   };
-
    const renderMagazineTrending = (data) => {
      const {
        thumbnail = '',
@@ -139,14 +111,7 @@ const MagazineContent: React.FunctionComponent<MangazineContentProps> = (props) 
            className={`magazine-container ${classes.container}`}
          >
            <div className='magazine-img'>
-             <LazyLoadImage
-               alt='magazine-header-img'
-               src={checkPathImg(thumbnail)}
-               effect='blur'
-               height={controlHeithImg()}
-               width={controlWidthImg()}
-               delayMethod
-             />
+             { loadingTab ? <CircularProgress /> : <img alt='ok' src={checkPathImg(thumbnail)} /> }
            </div>
            <div className={classes.magazineHeader}>
              <div className='magazine-title '>
